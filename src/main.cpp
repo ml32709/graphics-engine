d93f4dda9f396004137061e72f1490e6659aa9b8
Matt Low <matthewlowstx@gmail.com>
2024-09-13 14:12:03 -0600

    basic lighting chapter done

--- src/main.cpp
+++ src/main.cpp
@@ -148,19 +148,6 @@ int main()
         -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
     };
 
-    glm::vec3 cubePositions[] = {
-        glm::vec3( 0.0f,  0.0f,  0.0f), 
-        glm::vec3( 2.0f,  5.0f, -15.0f), 
-        glm::vec3(-1.5f, -2.2f, -2.5f),  
-        glm::vec3(-3.8f, -2.0f, -12.3f),  
-        glm::vec3( 2.4f, -0.4f, -3.5f),  
-        glm::vec3(-1.7f,  3.0f, -7.5f),  
-        glm::vec3( 1.3f, -2.0f, -2.5f),  
-        glm::vec3( 1.5f,  2.0f, -2.5f), 
-        glm::vec3( 1.5f,  0.2f, -1.5f), 
-        glm::vec3(-1.3f,  1.0f, -1.5f)  
-    };
-
     // buffer objects for cubes 
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
@@ -210,8 +197,8 @@ int main()
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
 
-        // float fps = 1.0f / deltaTime;
-        // std::cout << fps << std::endl;
+        float fps = 1.0f / deltaTime;
+        std::cout << fps << std::endl;
 
         // input
         processInput(window, deltaTime);
@@ -245,18 +232,6 @@ int main()
 
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
-
-        // for(unsigned int i = 0; i < 10; i++) {
-        //     glm::mat4 model = glm::mat4(1.0f);
-        //     model = glm::translate(model, cubePositions[i]);
-        //     float angle = 20.0f * i;
-        //     if(i % 3 == 0) {
-        //         angle = glfwGetTime() * 25.0f;
-        //     }
-        //     model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-        //     shaderProgram.setMat4("model", model);            
-        //     glDrawArrays(GL_TRIANGLES, 0, 36);
-        // }
 
         // lamp
         lightSourceShaderProgram.use();
